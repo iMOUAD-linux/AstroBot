477dcf076a6d9b29509f40d474982867ceec9126
M O U A D <mouadboulwafa8@gmail.com>
2025-12-04 20:35:20 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -176,7 +176,7 @@ async def verifie(ctx, member: discord.Member):
             )
             embed.add_field(name="1. Understanding Arena Leveling", value="Arena Leveling is a system that ranks members according to their activity", inline=False)
             embed.add_field(name="2. Complete Tasks", value=f"Complete the tasks to level up, You can get the tasks here : {bot.get_channel(1444259444199260282).mention}", inline=False)
-            embed.add_field(name="3. Rise to Warrior", value=f"Claim 1000xp to be a {discord.utils.get(ctx.guild.roles, id=1438574168269525105).mention}, This makes you an official combatant in our **ARENA**.", inline=False)
+            embed.add_field(name="3. Rise to Warrior", value=f"Reach 1000xp to be a {discord.utils.get(ctx.guild.roles, id=1438574168269525105).mention}, This makes you an official combatant in our **ARENA**.", inline=False)
             embed.set_thumbnail(url=member.display_avatar.url)
         await militesChannel.send(embed=embed)
     await ctx.reply(f"✔ {member.mention} has been **verified successfully!**")
