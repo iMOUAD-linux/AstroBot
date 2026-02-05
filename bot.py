783af2f291e9a8a85beb7fba0d895e257d05bcef
M O U A D <mouadboulwafa8@gmail.com>
2026-02-05 19:56:56 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -50,6 +50,7 @@ from dotenv import load_dotenv
 import os
 from datetime import timezone, datetime
 
+import bot2.py
 
 load_dotenv()
 token = os.getenv('TOKEN')
@@ -401,5 +402,3 @@ async def gameroles(ctx):
     await ctx.send(embed=embed, view=GameRoleView())
 
 bot.run(token)
-
-import bot2.py
